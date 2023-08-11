f9621e6aea03465b30b8c0916ab1a52fe8f415e6
ms-monirul <ms.monirul8579@gmail.com>
2023-08-11 22:22:43 +0600

    change something

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("VS code is Awesome...");
-        greeting("ms-monir");
+        greeting("Git")
 
     }
     public static void greeting(String name) {
-        System.out.println("Wellcom " + name);
+        System.out.println("Hello Hello " + name);
     }
 }
