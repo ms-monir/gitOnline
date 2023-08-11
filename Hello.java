6f870bc87b602b4a1dd8dd080c5491a75c947f5d
ms-monirul <ms.monirul8579@gmail.com>
2023-08-11 12:29:29 +0600

    added a new method called display hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("VS code is Awesome...");
+        greeting("ms-monir");
+
+    }
+    public static void greeting(String name) {
+        System.out.println("Wellcom " + name);
     }
 }
